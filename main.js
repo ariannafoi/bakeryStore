aca6c296a577864c53aaa1107a14ac4dc083c43b
Arianna Foi <ariannafoi@Air-di-Arianna.homenet.telecomitalia.it>
2023-11-22 23:17:56 +0100

    sistemato menu con colore navbar al click su hamburger

--- main.js
+++ main.js
@@ -1,30 +1,63 @@
+// const nav = document.querySelector('#nav');
+// const links = document.querySelectorAll('.a-link');
+// const check = document.querySelector('#check');
+
+
+// window.onscroll = ()=> {
+//   if(window.scrollY > 0){
+//     nav.classList.add('scrolled');
+
+//     links.forEach((link)=>{
+//       link.classList.add('link-color');
+//     });
+
+//   }else{
+//     nav.classList.remove('scrolled');
+
+//     links.forEach((link)=>{
+//       link.classList.remove('link-color');
+//     });
+//   }
+// }
+
+
 let nav = document.querySelector('#nav');
 const links = document.querySelectorAll('.a-link');
 const check = document.querySelector('#check');
 
-window.onscroll = ()=> {
-  if(window.scrollY > 0){
+
+check.addEventListener('click', ()=>{
+  if(check.checked){
     nav.classList.add('scrolled');
 
     links.forEach((link)=>{
       link.classList.add('link-color');
     });
-
   }else{
+    nav.classList.remove('scrolled');
+
+    links.forEach((link)=>{
+      link.classList.remove('link-color');
+    }); 
+  }
+})
+
+
+window.onscroll = ()=> {
+  if(window.scrollY == 0){
     nav.classList.remove('scrolled');
 
     links.forEach((link)=>{
       link.classList.remove('link-color');
     });
 
+  }else{
+    nav.classList.add('scrolled');
+
+    links.forEach((link)=>{
+      link.classList.add('link-color');
+    });
   }
 }
 
-check.addEventListener('click', ()=>{
-  if(check.checked){
-    nav.style.backgroundColor = 'white';
-    nav.style.transition = 'all .5s';
-  }else{
-    nav.style.backgroundColor = 'transparent'; 
-  }
-})
+
